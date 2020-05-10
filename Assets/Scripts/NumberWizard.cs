0202fb6d4f49b29312154c48d609bfb840308787
Sagar Deshpande 🐼 <sagad@amazon.com>
2020-05-10 11:10:58 -0700

    All changes related to NumberWizardUI udemy unity course.

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+    [SerializeField] int max;
+    [SerializeField] int min;
+
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    ArrayList previousGuesses;
+
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+    }
+
+    public void OnPressHigher() {
+        min = guess;
+        FindGuess();
+    }
+
+    public void OnPressLower() {
+        max = guess;
+        FindGuess();
+    }
+
+    private void StartGame() {
+        FindGuess();
+    }
+
+    private void FindGuess() {
+        guess = Random.Range(min, max + 1);
+        if(previousGuesses != null && previousGuesses.Contains(guess)) {
+            FindGuess();
+        } else {
+            if(previousGuesses == null) {
+                previousGuesses = new ArrayList();
+            }
+        }
+        previousGuesses.Add(guess);
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("Current scene: " + currentSceneIndex);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void EndGame() {
+        Application.Quit();
+    }
+}
